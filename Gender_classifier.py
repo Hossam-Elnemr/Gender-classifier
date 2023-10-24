7e57f2a1af270330d6ff625e455db54a29b96e87
Hossam Elnemr <87553099+Hossam-Elnemr@users.noreply.github.com>
2023-10-24 22:22:14 +0300

    Create Gender_classifier.py
    If a name matches male and female as (Nour) it returns (almost female) and may return male in other case.

---
+++ Gender_classifier.py
@@ -0,0 +1,20 @@
+import openpyxl
+import gender_guesser.detector as gender
+
+excel_file = input("Enter the name of the csv (excel) file: ")
+wrkbk = openpyxl.load_workbook(excel_file)
+sh = wrkbk.active
+d = gender.Detector(case_sensitive=False)
+
+column_number = input("Enter the number of the column that has the names: "
+
+# Loop over each row in the selected column that has the list names
+
+for i in range(1, sh.max_row+1):
+    column_number = 6
+    cell_obj = sh.cell(row=i, column = column_number)
+
+    if cell_obj.value is None or cell_obj.value == "":
+        continue
+    name = cell_obj.value.split()[0]
+    print(name + " : " + d.get_gender(name))
